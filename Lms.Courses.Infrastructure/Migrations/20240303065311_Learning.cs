7ae6af9e9d28409983cc5319244408cdcbf913ad
Permyakov Roman <r.permyakov@skbkontur.ru>
2024-03-03 11:53:35 +0500

    миграция по обучению

---
+++ Lms.Courses.Infrastructure/Migrations/20240303065311_Learning.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lms.Courses.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Learning : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Duration",
+                table: "Course",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.CreateTable(
+                name: "Learnings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    FinishedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Deadline = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    AssignedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Learnings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Progress",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LearningId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourseItemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ScoreInPercent = table.Column<int>(type: "integer", nullable: false),
+                    CommittedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Progress", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Progress_Learnings_LearningId",
+                        column: x => x.LearningId,
+                        principalTable: "Learnings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Progress_LearningId",
+                table: "Progress",
+                column: "LearningId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Progress");
+
+            migrationBuilder.DropTable(
+                name: "Learnings");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Course");
+        }
+    }
+}
